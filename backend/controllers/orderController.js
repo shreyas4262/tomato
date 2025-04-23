b82f4eb020e2d38b436729d11b334b3d9d76c2a2
shreyas4262 <shreyas9084@gmail.com>
2025-04-23 12:26:44 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,6 @@ const stripe =  new Stripe(process.env.STRIPE_SECRET_KEY)
 // Placing user order for frontend
 const placeOrder = async (req, res) =>{
 
-    const frontend_url = 'http://localhost:5173';
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
@@ -47,8 +46,7 @@ const placeOrder = async (req, res) =>{
         const session = await stripe.checkout.sessions.create({
             line_items:line_items,
             mode:'payment',
-            success_url:`${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`
+           
         })
 
         res.json({success:true, session_url:session.url})
